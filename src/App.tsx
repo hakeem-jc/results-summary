71bc7ffea9549f935da50e3a9561a1a24d8e61b9
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-04-29 11:21:25 +0200

    Update markup for score component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Score from './components/Score/Score';
+import iconMemory from './assets/images/icon-memory.svg';
+import iconReaction from './assets/images/icon-reaction.svg';
+import iconVerbal from './assets/images/icon-verbal.svg';
+import iconVisual from './assets/images/icon-visual.svg';
 
-function App() {
+const App = () => {
 
   return (
-    <main className='app'>
+    <main className='app__container'>
       <section className='results'>
         <div className='results__info'>
           <h1 className='results__title'>Your Results</h1>
@@ -15,10 +19,18 @@ function App() {
 
         <div className='results__summary'>
           <h1 className='results__title'>Summary</h1>
-          <Score/>
-          
+          <Score title={"Reaction"} color={'red'} icon={iconReaction} score={80}/>
+          <Score title={"Memory"} color={'yellow'} icon={iconMemory} score={92}/>
+          <Score title={"Verbal"} color={'green'} icon={iconVerbal} score={61}/>
+          <Score title={"Visual"} color={'purple'} icon={iconVisual} score={72}/>
+          <button>Continue</button>
         </div>
       </section>
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
     </main>
   )
 }
--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -1,16 +1,21 @@
 import { FC } from 'react';
 import './Score.css';
 
-interface ScoreProps {}
+interface ScoreProps {
+    color: string;
+    icon: string;
+    title: string;
+    score: number;
+}
 
-const Score:FC<ScoreProps> = () => {
+const Score:FC<ScoreProps> = (props) => {
     return (
         <div className='score__summary-item'>
             <div className='score__summary-left'>
-                <img className='score__summary-item__icon' src="https://via.placeholder.com/50" alt="icon" />
+                <img className='score__summary-item__icon' src={props.icon} alt="icon" />
                 <h3 className='score__summary-label'>Reaction</h3>
             </div>
-            <h3 className='score__summary-score'><span>80</span> / 100</h3>
+            <h3 className='score__summary-score'><span>{props.score}</span> / 100</h3>
         </div>
     );
 }
